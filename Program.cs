eb8672b306b1b444e9a61863a1276a5d4c9f61ab
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2020-01-20 05:06:17 +0900

    into dotnet core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TowerDangeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
